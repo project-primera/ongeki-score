ad7598ccf91106b18dec520f9857fa69a21b1ef4
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-08-05 23:27:50 +0900

    fix: 正しくjsonが吐き出せるように変更

--- Bookmarklet/src/getScore.ts
+++ Bookmarklet/src/getScore.ts
@@ -2,15 +2,15 @@
 import axios from '../node_modules/axios/index';
 
 (function () {
+  console.log("run");
+
   const NET_URL = "https://ongeki-net.com/ongeki-mobile/";
   const TOOL_URL = "https://example.net/";
 
   const PRODUCT_NAME = "Project Primera - getScore";
   const VERSION = 1.0;
 
-  console.log("run");
-
-  class PlayerData{
+  class PlayerData {
     trophy: string = "";
     level: number = 0;
     name: string = "";
@@ -23,21 +23,37 @@ import axios from '../node_modules/axios/index';
     comment: string = "";
     friend_code: number = 0;
 
-    constructor(){
-      this.getPlayerDataFromNet();
-      this.getFriendCodeDataFromNet();
+    toJSON() {
+      return {
+        trophy: this.trophy,
+        level: this.level,
+        name: this.name,
+        battle_point: this.battle_point,
+        rating: this.rating,
+        rating_max: this.rating,
+        money: this.money,
+        total_money: this.total_money,
+        total_play: this.total_play,
+        comment: this.comment,
+        friend_code: this.friend_code,
+      }
     }
 
-    private getPlayerDataFromNet(){
-      axios.get(NET_URL + 'home/playerDataDetail/', {
-      }).then((response) => {
-        this.parsePlayerData(response.data);
+    async getData() {
+      await this.getPlayerDataFromNet();
+      await this.getFriendCodeDataFromNet();
+    }
+
+    private async getPlayerDataFromNet() {
+      await axios.get(NET_URL + 'home/playerDataDetail/', {
+      }).then(async (response) => {
+        await this.parsePlayerData(response.data);
       }).catch(function (error) {
         //TODO: エラー処理書く
       });
     }
 
-    private parsePlayerData(html: string) {
+    private async parsePlayerData(html: string) {
       var parseHTML = $.parseHTML(html);
       this.trophy = $(parseHTML).find(".trophy_block").find("span").text();
       this.level = +$(parseHTML).find(".lv_block").find("span").text();
@@ -49,30 +65,30 @@ import axios from '../node_modules/axios/index';
       this.total_money = +$(parseHTML).find(".user_data_detail_block").find("td").eq(2).text().split("（")[1].replace(/累計 /g, "").replace(/）/g, "").replace(/,/g, "");
       this.total_play = +$(parseHTML).find(".user_data_detail_block").find("td").eq(5).text();
       this.comment = $(parseHTML).find(".comment_block").parent().text().replace(/	/g, "").replace("\n", "").replace("\n", "");
-      }
+    }
 
-    private getFriendCodeDataFromNet(){
-      axios.get(NET_URL + 'friend/userFriendCode/', {
-      }).then((response) => {
-        this.parseUserFriendCodeData(response.data);
+    private async getFriendCodeDataFromNet() {
+      await axios.get(NET_URL + 'friend/userFriendCode/', {
+      }).then(async (response) => {
+        await this.parseUserFriendCodeData(response.data);
       }).catch(function (error) {
         //TODO: エラー処理書く
       });
     }
 
-    private parseUserFriendCodeData(html: string) {
+    private async parseUserFriendCodeData(html: string) {
       var parseHTML = $.parseHTML(html);
       this.friend_code = +$(parseHTML).find(".friendcode_block").text();
     }
   }
 
-  class SongInfo{
+  class SongInfo {
     title: string;
     over_damage_high_score: number;
     battle_high_score: number;
     technical_high_score: number;
-    
-    constructor(title: string, over_damage_high_score: number, battle_high_score: number, technical_high_score: number){
+
+    constructor(title: string, over_damage_high_score: number, battle_high_score: number, technical_high_score: number) {
       this.title = title;
       this.over_damage_high_score = over_damage_high_score;
       this.battle_high_score = battle_high_score;
@@ -80,46 +96,57 @@ import axios from '../node_modules/axios/index';
     }
   }
 
-  class ScoreData{
+  class ScoreData {
     basicSongInfos = new Array<SongInfo>();
     advancedSongInfos = new Array<SongInfo>();
     expertInfos = new Array<SongInfo>();
     masterSongInfos = new Array<SongInfo>();
     lunaticSongInfos = new Array<SongInfo>();
 
-    constructor(){
-      this.getAllDifficultyScoreDataFromNet();
+    toJSON(){
+      return {
+        basicSongInfos: this.basicSongInfos,
+        advancedSongInfos: this.advancedSongInfos,
+        expertInfos: this.expertInfos,
+        masterSongInfos: this.masterSongInfos,
+        lunaticSongInfos: this.lunaticSongInfos,
+      };
     }
-    private getAllDifficultyScoreDataFromNet(){
-      [0, 1, 2, 3, 10].forEach((value, index, array) => {
-        this.getScoreHtmlFromNet(value);
+
+    async getData() {
+      await this.getAllDifficultyScoreDataFromNet();
+    }
+
+    private async getAllDifficultyScoreDataFromNet() {
+      await [0, 1, 2, 3, 10].forEach(async (value, index, array) => {
+        await this.getScoreHtmlFromNet(value);
       });
     }
 
-    private getScoreHtmlFromNet(difficulty: number){
-      axios.get(NET_URL + 'record/musicGenre/search/', {
+    private async getScoreHtmlFromNet(difficulty: number) {
+      await axios.get(NET_URL + 'record/musicGenre/search/', {
         params: {
           genre: 99,
           diff: difficulty
         }
-      }).then((response) => {
-        this.parseScoreData(response.data, difficulty);
+      }).then(async (response) => {
+        await this.parseScoreData(response.data, difficulty);
       }).catch(function (error) {
         //TODO: エラー処理書く
       });
     }
 
-    private parseScoreData(html: string, difficulty: number) {
+    private async parseScoreData(html: string, difficulty: number) {
       var parseHTML = $.parseHTML(html);
       var $innerContainer3 = $(parseHTML).find(".basic_btn");
-  
-      $innerContainer3.each((key, value) => {
+
+      await $innerContainer3.each((key, value) => {
         $(value).each((k, v) => {
           var song = new SongInfo(
             $(v).find(".music_label").text(),
-            +$($(v).find(".score_value")[0]).text(),
-            +$($(v).find(".score_value")[1]).text(),
-            +$($(v).find(".score_value")[2]).text()
+            +$($(v).find(".score_value")[0]).text().replace(/,/g, "").replace(/%/g, ""),
+            +$($(v).find(".score_value")[1]).text().replace(/,/g, ""),
+            +$($(v).find(".score_value")[2]).text().replace(/,/g, "")
           );
           switch (difficulty) {
             case 0: this.basicSongInfos.push(song); break;
@@ -133,41 +160,39 @@ import axios from '../node_modules/axios/index';
     }
   }
 
-  class TrophyInfo{
+  class TrophyInfo {
     name: string;
     detail: string;
 
-    constructor(name: string, detail: string){
+    constructor(name: string, detail: string) {
       this.name = name;
       this.detail = detail;
     }
   }
 
-  class TrophyData{
+  class TrophyData {
     normalTrophyInfos: Array<TrophyInfo> = new Array<TrophyInfo>();
     silverTrophyInfos: Array<TrophyInfo> = new Array<TrophyInfo>();
     goldTrophyInfos: Array<TrophyInfo> = new Array<TrophyInfo>();
     platinumTrophyInfo: Array<TrophyInfo> = new Array<TrophyInfo>();
 
-    constructor(){
-      this.getAllRankTrophyDataFromNet();
+    async getData() {
+      await this.getAllRankTrophyDataFromNet();
     }
 
-    private getAllRankTrophyDataFromNet(){
+    private async getAllRankTrophyDataFromNet() {
       axios.get(NET_URL + 'collection/trophy/', {
-      }).then((response) => {
-        this.parseAllTrophyData(response.data);
+      }).then(async (response) => {
+        await this.parseAllTrophyData(response.data);
       }).catch(function (error) {
         //TODO: エラー処理書く
-      });    
+      });
     }
 
-    private parseAllTrophyData(html: string){
+    private async parseAllTrophyData(html: string) {
       var parseHTML = $.parseHTML(html);
-  
-      ["Normal", "Silver", "Gold", "Platinum"].forEach((value, index, array) => {
-        var trophyArray = [];
-  
+
+      await ["Normal", "Silver", "Gold", "Platinum"].forEach(async (value, index, array) => {
         var $listDiv = $(parseHTML).find("#" + value + "List");
         $listDiv.find(".m_10").each((key, v) => {
           var trophy = new TrophyInfo(
@@ -175,77 +200,78 @@ import axios from '../node_modules/axios/index';
             $($(v).find(".detailText")).text()
           );
           switch (value) {
-            case "Normal":    this.normalTrophyInfos.push(trophy);  break;
-            case "Silver":    this.silverTrophyInfos.push(trophy);  break;
-            case "Gold":      this.goldTrophyInfos.push(trophy);    break;
-            case "Platinum":  this.platinumTrophyInfo.push(trophy); break;
+            case "Normal": this.normalTrophyInfos.push(trophy); break;
+            case "Silver": this.silverTrophyInfos.push(trophy); break;
+            case "Gold": this.goldTrophyInfos.push(trophy); break;
+            case "Platinum": this.platinumTrophyInfo.push(trophy); break;
           }
         });
-     });
+      });
     }
   }
 
-  class CharacterFriendlyData{
-    friendly: { [key: string]: number} = {};
-    constructor(){
-      this.getCharacterFriendlyDataFromNet();
+  class CharacterFriendlyData {
+    friendly: { [key: string]: number } = {};
+
+    async getData() {
+      await this.getCharacterFriendlyDataFromNet();
     }
 
-    private getCharacterFriendlyDataFromNet(){
-      axios.get(NET_URL + 'character/', {
-      }).then((response) => {
-        this.parseCharacterFriendlyData(response.data);
+    private async getCharacterFriendlyDataFromNet() {
+      await axios.get(NET_URL + 'character/', {
+      }).then(async (response) => {
+        await this.parseCharacterFriendlyData(response.data);
       }).catch(function (error) {
         //TODO: エラー処理書く
       });
     }
 
-    private parseCharacterFriendlyData(html: string) {
+    private async parseCharacterFriendlyData(html: string) {
       var parseHTML = $.parseHTML(html);
       var $chara_btn = $(parseHTML).find(".chara_btn");
-      $chara_btn.each((key, value) => {
+      await $chara_btn.each((key, value) => {
         var characterID: string = $(value).find("input").val() as string || "";
-  
-        var friendlyTensPlace: string = ($(value).find(".character_friendly_conainer").find("img").eq(1).attr('src') || "0").replace("https://ongeki-net.com/ongeki-mobile/img/friendly/num_", "").replace("0.png", "") ;
+
+        var friendlyTensPlace: string = ($(value).find(".character_friendly_conainer").find("img").eq(1).attr('src') || "0").replace("https://ongeki-net.com/ongeki-mobile/img/friendly/num_", "").replace("0.png", "");
         var friendlyUnitsPlace: string = ($(value).find(".character_friendly_conainer").find("img").eq(2).attr('src') || "0").replace("https://ongeki-net.com/ongeki-mobile/img/friendly/num_", "").replace(".png", "");
-  
+
         this.friendly[characterID] = +(friendlyTensPlace + friendlyUnitsPlace);
       });
     }
   }
 
-  class RecentMusicInfo{
+  class RecentMusicInfo {
     title: string = "";
     technicalScore: number = 0;
 
-    constructor(title: string, technicalScore: number){
+    constructor(title: string, technicalScore: number) {
       this.title = title;
       this.technicalScore = technicalScore;
     }
   }
 
-  class RatingRecentMusicData{
+  class RatingRecentMusicData {
     ratingRecentMusicObject: Array<RecentMusicInfo> = new Array<RecentMusicInfo>();
 
-    constructor(){
-      this.getRatingRecentMusicDataFromNet();
+    async getData() {
+      await this.getRatingRecentMusicDataFromNet();
     }
 
-    private getRatingRecentMusicDataFromNet() {
-      axios.get(NET_URL + 'home/ratingTargetMusic/', {
-      }).then((response) => {
-        this.parseRatingRecentMusicData(response.data);
+    private async getRatingRecentMusicDataFromNet() {
+      await axios.get(NET_URL + 'home/ratingTargetMusic/', {
+      }).then(async (response) => {
+        await this.parseRatingRecentMusicData(response.data);
       }).catch(function (error) {
         //TODO: エラー処理書く
-      }); 
+      });
     }
 
-    private parseRatingRecentMusicData(html: string) {
+    private async parseRatingRecentMusicData(html: string) {
       var parseHTML = $.parseHTML(html);
       var $basic_btn = $(parseHTML).find(".basic_btn");
       var count: number = 0;
-  
-      $basic_btn.each((key, value) => {
+
+      await $basic_btn.each((key, value) => {
         if ($(value).html().match(/TECHNICAL SCORE/)) {
           var info: RecentMusicInfo = new RecentMusicInfo(
             $(value).find(".music_label").text(),
@@ -256,28 +282,30 @@ import axios from '../node_modules/axios/index';
       });
     }
   }
-
-  var playerData: PlayerData = new PlayerData();
-  var scoreData: ScoreData = new ScoreData();
-  var trophyData: TrophyData= new TrophyData();
-  var characterFriendlyData: CharacterFriendlyData = new CharacterFriendlyData();
-  var ratingRecentMusicData: RatingRecentMusicData = new RatingRecentMusicData();
-
-  console.log(playerData);
-  console.log(scoreData);
-  console.log(trophyData);
-  console.log(characterFriendlyData);
-  console.log(ratingRecentMusicData);
-
-  var allData = {
-    player: playerData,
-    score: scoreData,
-    trophy: trophyData,
-    character: characterFriendlyData,
-    recent: ratingRecentMusicData,
+  class AllData {
+    playerData: PlayerData = new PlayerData();
+    scoreData: ScoreData = new ScoreData();
+    trophyData: TrophyData = new TrophyData();
+    characterFriendlyData: CharacterFriendlyData = new CharacterFriendlyData();
+    ratingRecentMusicData: RatingRecentMusicData = new RatingRecentMusicData();
   }
-  var json = JSON.stringify(allData);
 
-  console.log(allData);
-  console.log(json);
+
+  let main = async () => {
+    let allData: AllData = new AllData();
+
+    await allData.playerData.getData();
+    await allData.scoreData.getData();
+    await allData.trophyData.getData();
+    await allData.characterFriendlyData.getData();
+    await allData.ratingRecentMusicData.getData();
+
+    console.log(allData.scoreData.expertInfos[3]);
+    var json = JSON.stringify(allData);
+
+    console.log(json);
+  }
+
+  main();
+
 })();
