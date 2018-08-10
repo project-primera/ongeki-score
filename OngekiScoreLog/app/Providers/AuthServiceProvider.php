267db2997d7342b2e2d67a7e7f5f0c75d348e538
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-08-11 01:26:02 +0900

    feat: AuthServiceProviderにPassportの記述を追加

--- OngekiScoreLog/app/Providers/AuthServiceProvider.php
+++ OngekiScoreLog/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -23,6 +24,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Passport::routes();
         $this->registerPolicies();
 
         //
