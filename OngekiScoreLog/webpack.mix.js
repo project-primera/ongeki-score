93f563459e339cc52d6b93a0fb2b0387eb92675a
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-08-19 23:52:00 +0900

    chore: ビルドタスクの更新

--- OngekiScoreLog/webpack.mix.js
+++ OngekiScoreLog/webpack.mix.js
@@ -11,5 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sourceMaps()
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/laravel/app.scss', 'public/css')
+    .sass('resources/assets/sass/style.scss', 'public/css');   
