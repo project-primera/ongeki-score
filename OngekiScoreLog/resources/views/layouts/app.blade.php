a0255816caacba44aa70b826c0d136faf932aa28
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-08-19 22:31:25 +0900

    chore: Pure.cssを追加

--- OngekiScoreLog/resources/views/layouts/app.blade.php
+++ OngekiScoreLog/resources/views/layouts/app.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>アプリ名 - @yield('title')</title>
-    </head>
-    <body>
-        @section('sidebar')
-            ここがメインのサイドバー
-        @show
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>アプリ名 - @yield('title')</title>
+</head>
+<body>
+    @section('additonal_sidebar')
+        ここがメインのサイドバー
+    @show
 
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
 </html>
