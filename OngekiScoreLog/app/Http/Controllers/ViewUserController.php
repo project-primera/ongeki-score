3570f3b7e5c41af5c790bb33f85f344cdc607c0e
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-10-16 23:28:53 +0900

    feat: プレイヤーの曲統計を詳細モード時のみレベル表示するように

--- OngekiScoreLog/app/Http/Controllers/ViewUserController.php
+++ OngekiScoreLog/app/Http/Controllers/ViewUserController.php
@@ -19,34 +19,62 @@ class ViewUserController extends Controller
         $scoreData->addDetailedData();
         $score = $scoreData->value;
 
+        $submenuActive = [0 => "", 1 => "", 2 => "", 3 => ""];
+
+        switch (true) {
+            case ($mode === "technical"):
+                $mode = "song_status_technical";
+                $submenuActive[3] = "is-active";
+                break;
+            case ($mode === "battle"):
+                $mode = "song_status_battle";
+                $submenuActive[2] = "is-active";
+                break;
+            case ($mode === "details"):
+                $mode = "song_status_details";
+                $submenuActive[1] = "is-active";
+                break;
+            default:
+                $mode = "song_status";
+                $submenuActive[0] = "is-active";
+                break;
+        }
+
+        
         $stat = [
             "Basic" => [],
             "Advanced" => [],
             "Expert" => [],
             "Master" => [],
             "Lunatic" => [],
-            "1" => [],
-            "2" => [],
-            "3" => [],
-            "4" => [],
-            "5" => [],
-            "6" => [],
-            "7" => [],
-            "7+" => [],
-            "8" => [],
-            "8+" => [],
-            "9" => [],
-            "9+" => [],
-            "10" => [],
-            "10+" => [],
-            "11" => [],
-            "11+" => [],
-            "12" => [],
-            "12+" => [],
-            "13" => [],
-            "13+" => [],
-            "14" => [],
+
         ];
+        if($mode == "song_status_details"){
+            $stat += [
+                "1" => [],
+                "2" => [],
+                "3" => [],
+                "4" => [],
+                "5" => [],
+                "6" => [],
+                "7" => [],
+                "7+" => [],
+                "8" => [],
+                "8+" => [],
+                "9" => [],
+                "9+" => [],
+                "10" => [],
+                "10+" => [],
+                "11" => [],
+                "11+" => [],
+                "12" => [],
+                "12+" => [],
+                "13" => [],
+                "13+" => [],
+                "14" => [],
+            ];
+        }
+
 
 		
 
@@ -83,59 +111,40 @@ class ViewUserController extends Controller
             }
 			$stat[$value->difficulty_str]["fb"] += $value->full_bell;
             
-            
-            if($value->technical_high_score == 0){
-                $key = "NP";
-            }else if($value->technical_high_score < 850000){
-                $key = "B";
-            }else{
-                $key = $value->technical_high_score_rank;
-            }
-            if(!isset($stat[$value->level_str][$key])){
-				$stat[$value->level_str][$key] = 0;
-            }
-            $stat[$value->level_str][$key] += 1;
 
-            if(!isset($stat[$value->level_str][$value->over_damage_high_score_rank])){
-				$stat[$value->level_str][$value->over_damage_high_score_rank] = 0;
-            }
-            $stat[$value->level_str][$value->over_damage_high_score_rank] += 1;
+            if($mode == "song_status_details"){
+                if($value->technical_high_score == 0){
+                    $key = "NP";
+                }else if($value->technical_high_score < 850000){
+                    $key = "B";
+                }else{
+                    $key = $value->technical_high_score_rank;
+                }
+                if(!isset($stat[$value->level_str][$key])){
+                    $stat[$value->level_str][$key] = 0;
+                }
+                $stat[$value->level_str][$key] += 1;
 
-            if(!isset($stat[$value->level_str]["fc"])){
-				$stat[$value->level_str]["fc"] = 0;
-            }
-            $stat[$value->level_str]["fc"] += $value->full_combo;
-            
-            if(!isset($stat[$value->level_str]["ab"])){
-				$stat[$value->level_str]["ab"] = 0;
-            }
-            $stat[$value->level_str]["ab"] += $value->all_break;
-            
-            if(!isset($stat[$value->level_str]["fb"])){
-				$stat[$value->level_str]["fb"] = 0;
-            }
-			$stat[$value->level_str]["fb"] += $value->full_bell;
-        }
+                if(!isset($stat[$value->level_str][$value->over_damage_high_score_rank])){
+                    $stat[$value->level_str][$value->over_damage_high_score_rank] = 0;
+                }
+                $stat[$value->level_str][$value->over_damage_high_score_rank] += 1;
 
-        $submenuActive = [0 => "", 1 => "", 2 => "", 3 => ""];
-
-        switch (true) {
-            case ($mode === "technical"):
-                $mode = "song_status_technical";
-                $submenuActive[3] = "is-active";
-                break;
-            case ($mode === "battle"):
-                $mode = "song_status_battle";
-                $submenuActive[2] = "is-active";
-                break;
-            case ($mode === "details"):
-                $mode = "song_status_details";
-                $submenuActive[1] = "is-active";
-                break;
-            default:
-                $mode = "song_status";
-                $submenuActive[0] = "is-active";
-                break;
+                if(!isset($stat[$value->level_str]["fc"])){
+                    $stat[$value->level_str]["fc"] = 0;
+                }
+                $stat[$value->level_str]["fc"] += $value->full_combo;
+                
+                if(!isset($stat[$value->level_str]["ab"])){
+                    $stat[$value->level_str]["ab"] = 0;
+                }
+                $stat[$value->level_str]["ab"] += $value->all_break;
+                
+                if(!isset($stat[$value->level_str]["fb"])){
+                    $stat[$value->level_str]["fb"] = 0;
+                }
+                $stat[$value->level_str]["fb"] += $value->full_bell;
+            }
         }
 
         return view('user', compact('id', 'status', 'score', 'stat', 'mode', 'submenuActive'));
