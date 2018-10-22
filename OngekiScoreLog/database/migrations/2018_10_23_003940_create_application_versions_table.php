8fc20fba752d611de446bf88df01c4b359420e4d
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-10-23 00:47:42 +0900

    feat: アプリケーションのバージョンを管理するモデルを作成

---
+++ OngekiScoreLog/database/migrations/2018_10_23_003940_create_application_versions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationVersionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application_versions', function (Blueprint $table) {
+            $table->increments('tag_name');
+            $table->string('name');
+            $table->string('body');
+            $table->string('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application_versions');
+    }
+}
