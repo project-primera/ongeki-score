27d3a46e2e7e1f040a3fb0ea3345ce4799f256a2
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-10-28 04:47:47 +0900

    Revert "feat: faviconを追加"
    This reverts commit 10c6fed9066b008917af717234ce1b461684b6f6.

--- OngekiScoreLog/resources/views/layouts/app.blade.php
+++ OngekiScoreLog/resources/views/layouts/app.blade.php
@@ -7,48 +7,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/style.css">
-    
-        <meta name="msapplication-TileColor" content="#2d88ef">
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-        <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="favicons/favicon.ico">
-        <link rel="icon" type="image/vnd.microsoft.icon" href="favicons/favicon.ico">
-        <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="36x36" href="favicons/android-chrome-36x36.png">
-        <link rel="icon" type="image/png" sizes="48x48" href="favicons/android-chrome-48x48.png">
-        <link rel="icon" type="image/png" sizes="72x72" href="favicons/android-chrome-72x72.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="favicons/android-chrome-96x96.png">
-        <link rel="icon" type="image/png" sizes="128x128" href="favicons/android-chrome-128x128.png">
-        <link rel="icon" type="image/png" sizes="144x144" href="favicons/android-chrome-144x144.png">
-        <link rel="icon" type="image/png" sizes="152x152" href="favicons/android-chrome-152x152.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="favicons/android-chrome-192x192.png">
-        <link rel="icon" type="image/png" sizes="256x256" href="favicons/android-chrome-256x256.png">
-        <link rel="icon" type="image/png" sizes="384x384" href="favicons/android-chrome-384x384.png">
-        <link rel="icon" type="image/png" sizes="512x512" href="favicons/android-chrome-512x512.png">
-        <link rel="icon" type="image/png" sizes="36x36" href="favicons/icon-36x36.png">
-        <link rel="icon" type="image/png" sizes="48x48" href="favicons/icon-48x48.png">
-        <link rel="icon" type="image/png" sizes="72x72" href="favicons/icon-72x72.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="favicons/icon-96x96.png">
-        <link rel="icon" type="image/png" sizes="128x128" href="favicons/icon-128x128.png">
-        <link rel="icon" type="image/png" sizes="144x144" href="favicons/icon-144x144.png">
-        <link rel="icon" type="image/png" sizes="152x152" href="favicons/icon-152x152.png">
-        <link rel="icon" type="image/png" sizes="160x160" href="favicons/icon-160x160.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="favicons/icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="196x196" href="favicons/icon-196x196.png">
-        <link rel="icon" type="image/png" sizes="256x256" href="favicons/icon-256x256.png">
-        <link rel="icon" type="image/png" sizes="384x384" href="favicons/icon-384x384.png">
-        <link rel="icon" type="image/png" sizes="512x512" href="favicons/icon-512x512.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicons/icon-16x16.png">
-        <link rel="icon" type="image/png" sizes="24x24" href="favicons/icon-24x24.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicons/icon-32x32.png">
-        <link rel="manifest" href="favicons/manifest.json">
     </head>
 
     <body>
