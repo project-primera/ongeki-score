2079c70c9626fb37ee8053985949eda088330571
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-12-24 18:24:34 +0900

    fix: 難易度別スコアのLv14+が正しい位置に表示されない問題を修正 close #42 #41

--- OngekiScoreLog/app/Http/Controllers/ViewUserController.php
+++ OngekiScoreLog/app/Http/Controllers/ViewUserController.php
@@ -88,6 +88,7 @@ class ViewUserController extends Controller
             "Lv.13" => [],
             "Lv.13+" => [],
             "Lv.14" => [],
+            "Lv.14+" => [],
         ];
 
         $stat['average'] = $stat['level'];
