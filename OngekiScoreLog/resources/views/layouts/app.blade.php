906e0b19994e96d8c281a8fe336b52b5d482ccb1
Slime_hatena <Slime-hatena@users.noreply.github.com>
2018-12-28 02:54:07 +0900

    feat: フッターを追加

--- OngekiScoreLog/resources/views/layouts/app.blade.php
+++ OngekiScoreLog/resources/views/layouts/app.blade.php
@@ -132,6 +132,11 @@
                 @endcomponent
             </div>
         </main>
+
+        <footer>
+            This site is powered by <a href="https://github.com/Slime-hatena/ProjectPrimera" target="_blank">ProjectPrimera</a> licensed under the <a href="https://github.com/Slime-hatena/ProjectPrimera/blob/master/LICENSE" target="_blank">MIT</a>.
+        </footer>
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="{{ mix('/js/list.min.js') }}"></script>
         <script type="text/javascript" src="{{ mix('/js/sweet-scroll.min.js') }}"></script>
