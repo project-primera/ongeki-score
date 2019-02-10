dc55ad1df987daa4ac73ab30b04256d998a478df
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-02-10 14:29:11 +0900

    feat: 楽曲一覧ページの文言を追加

--- OngekiScoreLog/resources/views/music_list.blade.php
+++ OngekiScoreLog/resources/views/music_list.blade.php
@@ -9,10 +9,25 @@
     <script type="text/javascript" src="{{ mix('/js/sortMusic.js') }}"></script>
 @endsection
 
-
 @section('content')
 <div class="container">
     <article class="box">
+        <p>
+            譜面定数が青字斜体のものは推定値です。<br>
+            データをご提供頂ける場合は「ハンドルネーム, 楽曲名, 難易度, 譜面定数」を添えて、以下の連絡先からご連絡いただけますと幸いです。<br>
+            <a href="https://twitter.com/ongeki_score" target="_blank">Twitter@ongeki_score</a><br>
+            <a href="mailto:info&#64;ongeki-score.net">Mail: info&nbsp;(at)&nbsp;ongeki-score.net</a>
+        </p>
+        <p>
+            以下の譜面定数データは使用元の表記なく、誰でも使用することができます。<br>
+            但し、楽曲名などを始めとした著作物に対しての許諾ではございません。個人の判断でご利用ください。
+        </p>
+        <!--
+        <p>
+            データ御提供者様(順不同 敬称略)<br>
+        </p>
+        -->
+
         <div id="sort_table" class="table_wrap">
             <table class="table is-narrow user-progress-total-table music-list">
                 <thead>
@@ -23,6 +38,14 @@
                         <th class="sort" data-sort="sort_extra_level">譜面定数</th>
                     </tr>
                 </thead>
+                <tfoot>
+                    <tr>
+                        <th class="sort" data-sort="sort_title">タイトル</th>
+                        <th class="sort" data-sort="sort_difficulty">難易度</th>
+                        <th class="sort" data-sort="sort_level">レベル</th>
+                        <th class="sort" data-sort="sort_extra_level">譜面定数</th>
+                    </tr>
+                </tfoot>
                 <tbody class="list">
                     @foreach ($view as $key => $value)
                         <tr>
