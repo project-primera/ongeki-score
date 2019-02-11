77ba7bc8f10761784fed80fec28eed23c4e1a4cf
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-02-12 00:41:39 +0900

    feat: 譜面定数データ提供者様の御名前を記載

--- OngekiScoreLog/resources/views/music_list.blade.php
+++ OngekiScoreLog/resources/views/music_list.blade.php
@@ -13,22 +13,27 @@
 <div class="container">
     <article class="box">
         <p>
+            各ユーザーごとのレート値は"ユーザーページ>Technical"からご確認いただけます。
+        </p>
+        <p>
             譜面定数が青字斜体のものは推定値です。<br>
             データをご提供頂ける場合は「ハンドルネーム, 楽曲名, 難易度, 譜面定数」を添えて、以下の連絡先からご連絡いただけますと幸いです。<br>
             <a href="https://twitter.com/ongeki_score" target="_blank">Twitter@ongeki_score</a><br>
             <a href="mailto:info&#64;ongeki-score.net">Mail: info&nbsp;(at)&nbsp;ongeki-score.net</a>
         </p>
+        <div class="notification">
+            <p>
+                掲載されている譜面定数データは<b>以下のデータ御提供者様の御名前を併記することを条件として</b>、誰でも利用することができます。但し、楽曲名などを始めとした著作物に対しての許諾ではございません。個人の判断でご利用ください。
+            </p>
+        </div>
         <p>
-            以下の譜面定数データは使用元の表記なく、誰でも使用することができます。<br>
-            但し、楽曲名などを始めとした著作物に対しての許諾ではございません。個人の判断でご利用ください。
-        </p>
-        <!--
-        <p>
-            データ御提供者様(順不同 敬称略)<br>
-        </p>
-        -->
-        <p>
-            各ユーザーごとのレート値は"ユーザーページ>Technical"からご確認いただけます。
+            <b>データ御提供者様(順不同 敬称略)</b><br>
+            <ul>
+                <li><a href="https://twitter.com/Rinsaku471" target="_blank">Twitter@Rinsaku471</a></li>
+                <li><a href="https://twitter.com/RKS49019722" target="_blank">Twitter@RKS49019722</a></li>
+                <li><a href="https://twitter.com/masa_9713" target="_blank">Twitter@masa_9713</a></li>
+            </ul>
+            以上の皆様から頂いた情報をもとにしております。御提供ありがとうございました。
         </p>
 
         <div id="sort_table" class="table_wrap">
