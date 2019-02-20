3ab0780c8395102729bac4fab2738aaa0d4fa6c2
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-02-20 19:17:15 +0900

    feat: Slack botが起こしたエラーをSlackに通知しないように

--- OngekiScoreLog/app/Exceptions/Handler.php
+++ OngekiScoreLog/app/Exceptions/Handler.php
@@ -39,31 +39,33 @@ class Handler extends ExceptionHandler
     public function report(Exception $exception)
     {
         $user = Auth::user();
-        $content = $exception->getMessage() . "\n" . get_class($exception) . "\n" . url()->full();
-        $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\nReferer: " . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "N/A") . "\n\n";
-        if(!is_null($user)){
-            $fileContent .= "User:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\n";
-            $fields = ["File" => $exception->getFile(), "Line" => $exception->getLine(), "IP Address" => \Request::ip(), "User id" => $user->id];
-        }else{
-            $fileContent .= "User: N/A\n\n";
-            $fields = ["File" => $exception->getFile(), "Line" => $exception->getLine(), "IP Address" => \Request::ip(), "User id" => "N/A"];
+        $referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "N/A";
+        if(strpos($referrer, "+https://api.slack.com/robots") === false){
+            $content = $exception->getMessage() . "\n" . get_class($exception) . "\n" . url()->full();
+            $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\nReferer: " . $referrer . "\n\n";
+            if(!is_null($user)){
+                $fileContent .= "User:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\n";
+                $fields = ["File" => $exception->getFile(), "Line" => $exception->getLine(), "IP Address" => \Request::ip(), "User id" => $user->id];
+            }else{
+                $fileContent .= "User: N/A\n\n";
+                $fields = ["File" => $exception->getFile(), "Line" => $exception->getLine(), "IP Address" => \Request::ip(), "User id" => "N/A"];
+            }
+            $fileContent .= "Cookie:\n" . var_export(Cookie::get(), true) . "\n\nRequest:\n" . var_export(Request::all(), true) . "\n\n" . $exception->__toString();
+    
+            switch (true) {
+                case ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException):
+                    Slack::Warning($content, $fileContent, $fields, "warning");
+                    break;
+                
+                case ($exception instanceof \Illuminate\Auth\AuthenticationException):
+                    Slack::Notice($content, $fileContent, $fields, "warning");
+                    break;
+                
+                default:
+                    Slack::Critical($content, $fileContent, $fields, "error");
+                    break;
+            }
         }
-        $fileContent .= "Cookie:\n" . var_export(Cookie::get(), true) . "\n\nRequest:\n" . var_export(Request::all(), true) . "\n\n" . $exception->__toString();
-
-        switch (true) {
-            case ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException):
-                Slack::Warning($content, $fileContent, $fields, "warning");
-                break;
-            
-            case ($exception instanceof \Illuminate\Auth\AuthenticationException):
-                Slack::Notice($content, $fileContent, $fields, "warning");
-                break;
-            
-            default:
-                Slack::Critical($content, $fileContent, $fields, "error");
-                break;
-        }
-        
         parent::report($exception);
     }
 
