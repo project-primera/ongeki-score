5ca2a01e2ef4763593e8b8150799b1085cab9a01
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-02-20 19:57:04 +0900

    fix: 非ログイン時は閲覧できないページのメニューを表示しないように

--- OngekiScoreLog/resources/views/layouts/components/sidebar.blade.php
+++ OngekiScoreLog/resources/views/layouts/components/sidebar.blade.php
@@ -10,23 +10,21 @@
         <li><a href="/random" class="@yield('sidemark_random')">ランダムアクセス</a></li>
         <li><a href="/music" class="@yield('sidemark_music')">楽曲リスト/譜面定数</a></li>
     </ul>
-    <p class="menu-label">マイページ</p>
-    <ul class="menu-list">
-        <li><a href="/mypage">簡易表示</a></li>
-        <li><a href="/mypage/details">詳細表示</a></li>
-        <li><a href="/mypage/battle">Battle Score</a></li>
-        <li><a href="/mypage/technical">Technical Score</a></li>
-        <li><a href="/mypage/progress">更新差分</a></li>
-    </ul>
-    <p class="menu-label">設定</p>
-    <ul class="menu-list">
-        <li>
-            <ul>
-                <li><a href="/bookmarklet" class="@yield('sidemark_bookmarklet')">ブックマークレットを取得する</a></li>
-                <li><a href="/setting" class="@yield('sidemark_setting')">その他の設定</a></li>
-            </ul>
-        </li>
-    </ul>
+    @if (Auth::check())
+        <p class="menu-label">マイページ</p>
+        <ul class="menu-list">
+            <li><a href="/mypage" class="@yield('sidemark_mypage_default')">簡易表示</a></li>
+            <li><a href="/mypage/details" class="@yield('sidemark_mypage_details')">詳細表示</a></li>
+            <li><a href="/mypage/battle" class="@yield('sidemark_mypage_battle')">Battle Score</a></li>
+            <li><a href="/mypage/technical" class="@yield('sidemark_mypage_technical')">Technical Score</a></li>
+            <li><a href="/mypage/progress" class="@yield('sidemark_mypage_progress')">更新差分</a></li>
+        </ul>
+        <p class="menu-label">設定</p>
+        <ul class="menu-list">
+            <li><a href="/bookmarklet" class="@yield('sidemark_bookmarklet')">ブックマークレットを取得する</a></li>
+            <li><a href="/setting" class="@yield('sidemark_setting')">その他の設定</a></li>
+        </ul>
+    @endif
     <ul class="menu-list">
         <li><a href="/eula" class="@yield('sidemark_eula')">利用規約<br>プライバシーポリシー</a></li>
     </ul>
