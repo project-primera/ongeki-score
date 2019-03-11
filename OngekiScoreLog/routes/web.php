6f658d167c240b49fc5af143b2981c4a513f69b5
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-03-12 01:29:36 +0900

    feat: ユーザーの称号一覧ページを追加 close #23

--- OngekiScoreLog/routes/web.php
+++ OngekiScoreLog/routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', 'SimpleViewController@getIndex');
 
 Route::get('/user/{id}/progress', 'ViewUserProgressController@getIndex')->where(['id' => '\d+']);
 Route::get('/user/{id}/rating', 'ViewUserRatingController@getIndex')->where(['id' => '\d+']);
+Route::get('/user/{id}/trophy', 'ViewUserTrophyController@getIndex')->where(['id' => '\d+']);
 Route::get('/user/{id}/{mode?}', 'ViewUserController@getUserPage')->where(['id' => '\d+']);
 
 Route::get('/music', 'ViewMusicExtraLevelController@getIndex');
