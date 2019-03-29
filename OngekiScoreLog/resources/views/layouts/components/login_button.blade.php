af621da52f1dfa34951d1dbd80971f1de1fdf49c
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-03-29 19:25:23 +0900

    feat: bulma.ioアップデートに対する調整 close #69

--- OngekiScoreLog/resources/views/layouts/components/login_button.blade.php
+++
@@ -1,6 +0,0 @@
-@if (is_null(\Auth::user()))
-    <a href="/register"><i class="fas fa-user"></i>&nbsp;新規登録&nbsp;</a>
-    <a href="/login">&nbsp;ログイン&nbsp;</a>
-@else
-    <a href="/logout"><i class="fas fa-user"></i>&nbsp;ログアウト&nbsp;</a>
-@endif
