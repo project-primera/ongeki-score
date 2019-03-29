df545b4c9556d818f227612fb60bcb63ac8110d5
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-03-29 21:25:07 +0900

    feat: パスワード再設定メールの日本語化

---
+++ OngekiScoreLog/app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->from(config('mail.username'), config('app.name'))
+            ->subject('パスワードの再設定')
+            ->line('パスワード再発行のリクエストがありましたのでメールをお送りしました。')
+            ->action('パスワード再設定', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('もし心当たりがない場合は info@ongeki-score.net へ、本メッセージを転送いただけますと幸いです。');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
