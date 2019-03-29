df847fc04976fa5dc93435b87a67b25af913756b
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-03-29 20:32:43 +0900

    feat: 日本語用言語ファイルを追加

---
+++ OngekiScoreLog/resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '登録情報が見つかりませんでした。入力内容をご確認ください。',
+    'throttle' => 'ログイン試行回数が多いためしばらくログインできません。:seconds秒後にお試しください。',
+
+];
---
+++ OngekiScoreLog/resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードは6文字以上で、確認欄と同じ内容を入力してください。',
+    'reset' => 'パスワードを再設定しました！',
+    'sent' => 'パスワード再設定用のURLをメールでお送りしました。',
+    'token' => 'パスワード再設定用のトークンが不正です。再度お試しください。',
+    'user' => "メールアドレスに一致するユーザーが存在しませんでした。入力をご確認ください。",
+
+];
