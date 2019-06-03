31f310825ef921e699ff346cdbd75100f46ccaba
Slime_hatena <Slime-hatena@users.noreply.github.com>
2019-06-04 00:46:42 +0900

    fix: リバースプロキシ使用中にformのurlがhttpになってしまう不具合を修正

--- OngekiScoreLog/app/Http/Middleware/TrustProxies.php
+++ OngekiScoreLog/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = [
+        '127.0.0.0/8',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
