6ae166b0fc0a20f4ef2971e8ce01386e8642a09d
Slime_hatena <git@aki-memo.net>
2020-02-22 02:33:46 +0900

    feat: v2スコア登録のルートを追加

---
+++ OngekiScoreLog/app/Http/Controllers/api/v2/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api\v2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function PostUpdate(){
+        //
+    }
+}
