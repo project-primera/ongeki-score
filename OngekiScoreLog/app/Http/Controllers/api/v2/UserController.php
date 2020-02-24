ebb27bfa4a0df6c6ebeb95aab0048724aaed19fb
Slime_hatena <git@aki-memo.net>
2020-02-25 02:24:22 +0900

    fix: slackの処理が間違っている問題を修正

--- OngekiScoreLog/app/Http/Controllers/api/v2/UserController.php
+++ OngekiScoreLog/app/Http/Controllers/api/v2/UserController.php
@@ -29,15 +29,15 @@ class UserController extends Controller{
             $result['message'][] = "<p>只今メンテナンスを行っています。スコアデータの登録は行なえません。</p><p>詳細は<a href='https://twitter.com/ongeki_score' target='_blank' style='color:#222'>Twitter@ongeki_score</a>にてお知らせします。</p>";
 
             $user = Auth::user();
-            if($request->input('PlayerData') !== null){
-                $name = $request->input('PlayerData')['name'];
+            if(Auth::user()->name !== null){
+                $name = Auth::user()->name;
             }else{
                 $name = "<Unknown>";
             }
             $content = "スコア登録: " . $name . "(" . $user->id . ")\n" . url("/user/" . $user->id);
-            $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\nUser:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\nCookie:\n" . var_export(Cookie::get(), true) . "\n\nRequest:\n" . var_export($request, true);
+            $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\nUser:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\nRequest:\n" . var_export($request, true);
             $fields = ["IP Address" => \Request::ip(), "User id" => $user->id];
-            Slack::Warning($content, $fileContent, $fields, "success");
+            \App\Facades\Slack::Warning($content, $fileContent, $fields, "success");
             return $result;
         }
         $result['message'] = 'ok';
@@ -73,15 +73,15 @@ class UserController extends Controller{
             $result['message'][] = "<p>只今メンテナンスを行っています。スコアデータの登録は行なえません。</p><p>詳細は<a href='https://twitter.com/ongeki_score' target='_blank' style='color:#222'>Twitter@ongeki_score</a>にてお知らせします。</p>";
 
             $user = Auth::user();
-            if($request->input('PlayerData') !== null){
-                $name = $request->input('PlayerData')['name'];
+            if(Auth::user()->name !== null){
+                $name = Auth::user()->name;
             }else{
                 $name = "<Unknown>";
             }
-            $content = "スコア登録: " . $name . "(" . $user->id . ")\n" . url("/user/" . $user->id);
-            $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\nUser:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\nCookie:\n" . var_export(Cookie::get(), true) . "\n\nRequest:\n" . var_export($request, true);
-            $fields = ["IP Address" => \Request::ip(), "User id" => $user->id];
-            Slack::Warning($content, $fileContent, $fields, "success");
+            $content = "スコア登録(post): " . $name . "(" . $user->id . ")\n" . url("/user/" . $user->id);
+            $fileContent = "ip: " . \Request::ip() . "\nUser agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\nUser:\nid: " . $user->id . "\nemail: " . $user->email . "\nrole: " . $user->role . "\n\nRequest:\n" . var_export($request, true);
+            $fields = ["IP Address" => \Request::ip(), "User id" => $user->id];Cookie
+            \App\Facades\Slack::Warning($content, $fileContent, $fields, "success");
             return $result;
         }
 
