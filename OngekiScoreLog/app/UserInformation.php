f6dce9f95f877e0df559c41cc65f5349fb421c89
Slime_hatena <git@aki-memo.net>
2020-02-25 00:25:58 +0900

    fix: メソッドの命名を修正

--- OngekiScoreLog/app/UserInformation.php
+++ OngekiScoreLog/app/UserInformation.php
@@ -8,11 +8,11 @@ class UserInformation extends Model{
     protected $table = "user_informations";
     protected $guarded = ['id'];
 
-    public static function isStandardPlan($userId){
+    public static function IsStandardPlan($userId){
         $u = self::where('user_id', $userId)->first();
         return ($u !== null && $u->is_standard_plan !== 0);
     }
-    public static function isPremiumPlan($userId){
+    public static function IsPremiumPlan($userId){
         $u = self::where('user_id', $userId)->first();
         return ($u !== null && $u->is_premium_plan !== 0);
     }
