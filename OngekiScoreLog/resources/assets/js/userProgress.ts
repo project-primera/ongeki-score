21f491b332c53ea76367aad27837b45a321645ed
Slime_hatena <git@aki-memo.net>
2020-03-15 17:42:24 +0900

    feat: 画像生成中にページ上部から移動できないように

--- OngekiScoreLog/resources/assets/js/userProgress.ts
+++ OngekiScoreLog/resources/assets/js/userProgress.ts
@@ -2,7 +2,7 @@ var progress = document.querySelectorAll(".user-progress");
 let images = progress.length;
 let renderingState = 0;
 
-async function convert(element: HTMLElement, index: number){
+async function convert(element: HTMLElement, index: number) {
     let pr: Html2CanvasPromise<HTMLCanvasElement> = html2canvas(element, {
         width: 640,
     });
@@ -19,7 +19,7 @@ async function convert(element: HTMLElement, index: number){
         $('.progress').val(renderingState / images * 100);
         $(".progress-message").text("画像化中: " + renderingState + "/" + images + "(" + Math.round(renderingState / images * 100) + "%)");
 
-        if(images - 1 <= renderingState){
+        if (images - 1 <= renderingState) {
             // $('.user-progress').css('width','auto');
             $('.progress').val(100);
             $(".progress-message").text("ツイート中・・・");
@@ -31,19 +31,23 @@ async function convert(element: HTMLElement, index: number){
     });
 }
 
-$(function($) {
-    $('#select-generation').change(function() {
+$(function ($) {
+    $('#select-generation').change(function () {
         $('.select-generations-option').prop("disabled", true);
         $('#select-generation').addClass("is-loading");
         location.href = $('#current-url').text() + "/" + $('#select-generation  option:selected').val();
     });
-    
-    $('#submit_button').click(async function(){
+
+    $('#submit_button').click(async function () {
         $('#submit_button').prop("disabled", true);
-        $('.user-progress').css('width','640px');
+        $('.user-progress').css('width', '640px');
+        $('html').css('overflow', 'hidden');
+        $('body').css('overflow', 'hidden');
+        window.scrollTo(0, 0);
+        document.addEventListener('touchmove', function (e) { e.preventDefault(); }, { passive: false });
 
         $(".progress-message").text("画像化中: " + renderingState + "/" + images + "(0%)");
-        
+
         for (let index = 0; index < progress.length; index++) {
             const element: HTMLElement = <HTMLScriptElement>progress[index];
 
