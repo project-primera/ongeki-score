f3956f222bbc625b0cbfe32717122b52183a64a5
Slime-hatena <git@aki-memo.net>
2023-01-31 02:10:56 +0900

    feat: 譜面定数データの情報元記載を変更

--- OngekiScoreLog/resources/views/music_list.blade.php
+++ OngekiScoreLog/resources/views/music_list.blade.php
@@ -12,27 +12,12 @@
 @section('content')
     <article class="box">
         <p>
-            各ユーザーごとのレート値は"ユーザーページ>Technical"からご確認いただけます。
-        </p>
-        <p>
-            譜面定数が灰斜体のものは推定値です。<br>
-            データをご提供頂ける場合は「ハンドルネーム, 楽曲名, 難易度, 譜面定数」を添えて、以下の連絡先からご連絡いただけますと幸いです。<br>
-            <a href="https://twitter.com/ongeki_score" target="_blank">Twitter@ongeki_score</a>
+            各ユーザーごとのレート値は"ユーザーページ>Technical"からご確認いただけます。<br>
+            譜面定数が灰斜体のものは推定値です。
         </p>
         <div class="notification">
-            <p>
-                掲載されている譜面定数データは<b>以下のデータ御提供者様の御名前を併記することを条件として</b>、誰でも利用することができます。但し、楽曲名などを始めとした著作物に対しての許諾ではございません。個人の判断でご利用ください。
-            </p>
+            掲載されている譜面定数データは、<a href="https://twitter.com/ongeki_level" target="_blank">オンゲキ譜面定数部</a>様のデータを活用しております。
         </div>
-        <p>
-            <b>データ御提供者様(順不同 敬称略)</b><br>
-            <ul>
-                <li><a href="https://twitter.com/Rinsaku471" target="_blank">Twitter@Rinsaku471</a></li>
-                <li><a href="https://twitter.com/RKS49019722" target="_blank">Twitter@RKS49019722</a></li>
-                <li><a href="https://twitter.com/masa_9713" target="_blank">Twitter@masa_9713</a></li>
-            </ul>
-            以上の皆様から頂いた情報をもとにしております。御提供ありがとうございました。
-        </p>
 
         <div id="sort_table" class="table_wrap">
             <table class="table is-narrow user-progress-total-table music-list">
