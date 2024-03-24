0d95d3191ba6fd1d30caca80e10122a648c89388
Slime-hatena <git@aki-memo.net>
2024-03-25 04:46:05 +0900

    feat: レーティング候補曲が存在しない場合はテーブルを表示しないように

--- OngekiScoreLog/resources/views/layouts/components/user_rating/rating_best_table.blade.php
+++ OngekiScoreLog/resources/views/layouts/components/user_rating/rating_best_table.blade.php
@@ -49,47 +49,52 @@
 <p>スコアを上げることでレーティング対象曲入りを望める楽曲です。<br>
 記録されているスコアのうち、レート差が1.0以上の楽曲のみ表示されます。</p>
 <div class="table_wrap scalable">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th><abbr title="Difficulty">Dif</abbr></th>
-                <th>Lv</th>
-                <th><abbr title="Technical Score">TS</abbr></th>
-                <th>Rate</th>
-                <th><abbr title="同枠楽曲のレート値で一番低いものとの差です">レート差</abbr></th>
-                <th><abbr title="レーティング対象曲に入るために必要なスコアです">必要スコア</abbr></th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <th>Title</th>
-                <th><abbr title="Difficulty">Dif</abbr></th>
-                <th>Lv</th>
-                <th><abbr title="Technical Score">TS</abbr></th>
-                <th>Rate</th>
-                <th><abbr title="同枠楽曲のレート値で一番低いものとの差です">レート差</abbr></th>
-                <th><abbr title="レーティング対象曲に入るために必要なスコアです">必要スコア</abbr></th>
-            </tr>
-        </tfoot>
-        <tbody>
-            @for ($i = $targetCount; $i < $end; $i++)
-                @if($array[$i]->minDifferenceScore !== "" && $array[$i]->technical_high_score !== 0 && $array[$i]->minDifferenceRate >= -1.0)
-                    <tr>
-                        @if (array_key_exists($i, $array) && array_key_exists('song_id', $array[$i]))
-                            <td class="sort_title"><a href="{{url("/user/" . $id . "/music/" . $array[$i]->song_id . "/" . strtolower($array[$i]->difficulty_str))}}">{{$array[$i]->title}}</a></td>
-                        @else
-                            <td class="sort_title">{{$array[$i]->title}}</td>
-                        @endif
-                        <td>{{substr($array[$i]->difficulty_str, 0, 3)}}</td>
-                        <td>{!!$array[$i]->extraLevelStr!!}</td>
-                        <td>{{number_format($array[$i]->technical_high_score)}}</td>
-                        <td>{!!$array[$i]->ratingValue!!}</td>
-                        <td>{{sprintf("%.2f", $array[$i]->minDifferenceRate)}}</td>
-                        <td>{{$array[$i]->minDifferenceScore}}</td>
-                    </tr>
-                @endif
-            @endfor
-        </tbody>
-    </table>
+    @if ($targetCount == $end)
+        <p><b>レーティング対象外の候補曲はありません。</b></p>
+    @else
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th><abbr title="Difficulty">Dif</abbr></th>
+                    <th>Lv</th>
+                    <th><abbr title="Technical Score">TS</abbr></th>
+                    <th>Rate</th>
+                    <th><abbr title="同枠楽曲のレート値で一番低いものとの差です">レート差</abbr></th>
+                    <th><abbr title="レーティング対象曲に入るために必要なスコアです">必要スコア</abbr></th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th>Title</th>
+                    <th><abbr title="Difficulty">Dif</abbr></th>
+                    <th>Lv</th>
+                    <th><abbr title="Technical Score">TS</abbr></th>
+                    <th>Rate</th>
+                    <th><abbr title="同枠楽曲のレート値で一番低いものとの差です">レート差</abbr></th>
+                    <th><abbr title="レーティング対象曲に入るために必要なスコアです">必要スコア</abbr></th>
+                </tr>
+            </tfoot>
+            <tbody>
+                @for ($i = $targetCount; $i < $end; $i++)
+                    @if($array[$i]->minDifferenceScore !== "" && $array[$i]->technical_high_score !== 0 && $array[$i]->minDifferenceRate >= -1.0)
+                        <tr>
+                            @if (array_key_exists($i, $array) && array_key_exists('song_id', $array[$i]))
+                                <td class="sort_title"><a href="{{url("/user/" . $id . "/music/" . $array[$i]->song_id . "/" . strtolower($array[$i]->difficulty_str))}}">{{$array[$i]->title}}</a></td>
+                            @else
+                                <td class="sort_title">{{$array[$i]->title}}</td>
+                            @endif
+                            <td>{{substr($array[$i]->difficulty_str, 0, 3)}}</td>
+                            <td>{!!$array[$i]->extraLevelStr!!}</td>
+                            <td>{{number_format($array[$i]->technical_high_score)}}</td>
+                            <td>{!!$array[$i]->ratingValue!!}</td>
+                            <td>{{sprintf("%.2f", $array[$i]->minDifferenceRate)}}</td>
+                            <td>{{$array[$i]->minDifferenceScore}}</td>
+                        </tr>
+                    @endif
+                @endfor
+            </tbody>
+        </table>
+    @endif
 </div>
