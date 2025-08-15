ca63a0173b55205ee7a0e3ddc7aff8f0a2c2929a
Robert <147982436+WorldRobertProject@users.noreply.github.com>
2025-08-15 12:56:26 +0900

    privateモードを追加

--- OngekiScoreLog/app/Http/Controllers/Auth/RegisterController.php
+++ OngekiScoreLog/app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'private' => ($data['private'] ? 1 : 0),
         ]);
     }
 }
