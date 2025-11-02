ffe6425b4f70e24a7db6790794df5e19f3d8080a
Slime-hatena <git@aki-memo.net>
2025-11-03 03:13:23 +0900

    feat: プライベートモードの説明を変更
    「現在の状態:」を向こうで出さないのは画面の変化をつけてステートをわかりやすくする意図

--- OngekiScoreLog/resources/views/setting.blade.php
+++ OngekiScoreLog/resources/views/setting.blade.php
@@ -7,13 +7,26 @@
 @section('content')
     <article class="box">
         <h3 class="title is-3">プライベートモード</h3>
-        <p>プライベートモードにすると他人から見えなくなります。<br>
-            現在の状態: {{$display['private'] ? 'プライベート' : 'パブリック'}}モード<br>
-
+        <p>
+            プライベートモードにすると自分のユーザーページが他のユーザーから閲覧できなくなります。
+        </p>
+        <p>
+        <ul>
+            <li>・「すべてのユーザー」に表示されなくなります</li>
+            <li>・スコアページやレーティングページ、獲得称号ページが自分以外のユーザーから見えなくなります</li>
+            <li>・副作用として、OngekiScoreLogのデータを利用した外部サイトが利用できなくなるかもしれません</li>
+            <li>※統計情報の集計対象からは外れません</li>
+            <li>※管理者はユーザーサポートのためプライベートモードのユーザーページにアクセスする可能性があります</li>
+        </ul>
+        </p>
+        <p>
+            {{$display['private'] ? '現在の状態: 有効' : ''}}
+        </p>
+        <p>
             @if ($display['private'])
-                <a href="/setting/public" class="button">パブリックモードに戻す</a>
+                <a href="/setting/public" class="button is-danger">プライベートモードを無効にする</a>
             @else
-                <a href="/setting/private" class="button">プライベートモードにする</a>
+                <a href="/setting/private" class="button is-info">プライベートモードを有効にする</a>
             @endif
         </p>
     </article>
