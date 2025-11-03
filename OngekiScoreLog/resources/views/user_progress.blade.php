f4f43e50f6bf0b1cc9f8045d1b6ac3b4219f349a
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2025-11-03 20:03:12 +0900

    docs: コメントアウトの修正

--- OngekiScoreLog/resources/views/user_progress.blade.php
+++ OngekiScoreLog/resources/views/user_progress.blade.php
@@ -164,7 +164,7 @@ $difficulties = ['Total', 'Basic', 'Advanced', 'Expert', 'Master', 'Lunatic'];
                                 <span class="score-title">Platinum Score</span>
                                 <span class="score">{{number_format($value['new']->platinum_score)}}</span>
                                 <span class="difference">{{$value['difference']['platinum_score']}}</span>
-                                {{-- new-normal-ratingが空=該当ユーザーがプレミアムでない 場合そもそもレーティング情報を表示しない --}}
+                                {{-- 該当ユーザーがプレミアムでない場合そもそもレーティング情報を表示しない --}}
                                 @if($isPremium)
                                     <br>
                                     <span class="score-title">Single Rating</span>
